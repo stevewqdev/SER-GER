0c29427c291f26fcd6e8fd91c5bb6c2bd1cf0fbe
Steve Quezada <stevewqdev@gmail.com>
2019-02-09 22:20:55 -0400

    Se agregaron nuevas vistas
    Ademas se acomodaron detalles en el CSS.

---
+++ dist/js/functions.js
@@ -0,0 +1,31 @@
+// Functios for Project Management view
+$('.type-profile-selector .btn-white').click(function(){
+    if(!$(this).hasClass('active')){
+        $('.type-profile-selector .btn-white').removeClass('active');
+        $(this).addClass('active');
+    }
+})
+$('.project-filter .item').click(function(){
+    if(!$(this).hasClass('active')){
+        $('.project-filter .item').removeClass('active');
+        $(this).addClass('active');
+    }
+})
+//Functions for Profile
+$('.btn-add').click(function(){
+    var attrId = $(this).attr('data-id');
+    if($(`#${attrId}`).hasClass('disabled')){
+        $(`#${attrId}`).removeClass('disabled').addClass('enabled');
+    }else{
+        $(`#${attrId}`).removeClass('enabled').addClass('disabled');
+    }
+})
+$('.close-perk').click(function(){
+    var attrId = $(this).attr('data-id');
+    console.log(attrId);
+    if($(`#${attrId}`).hasClass('disabled')){
+        $(`#${attrId}`).removeClass('disabled').addClass('enabled');
+    }else{
+        $(`#${attrId}`).removeClass('enabled').addClass('disabled');
+    }
+})
